a1df3b27d91aa87c8438e644ca6698e86da63144
jinoxst <jinoxst@gmail.com>
2017-08-14 13:56:57 +0900

    update bug-fix item detail

--- src/App.js
+++ src/App.js
@@ -1,74 +1,12 @@
 import React from 'react';
-import { 
-  ActivityIndicator, 
-  AppRegistry, 
-  View, 
-  ListView, 
-  StyleSheet, 
-  Text } from 'react-native';
-import Row from './container/Row';
-import Config from './container/Config';
-import Header from './container/Header';
-import Footer from './container/Footer';
+import { AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import MainItemList from './container/MainItemList';
+import ItemDetail from './container/ItemDetail';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-  },
-  separator: {
-    flex: 1,
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#8E8E8E',
-  },
+const App = StackNavigator({
+  MainItemList: { screen: MainItemList },
+  ItemDetail:   { screen: ItemDetail },
 });
 
-class ListViewDemo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true
-    }
-  }
-
-  componentDidMount() {
-    return fetch(Config.LIST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        // console.log(responseData);
-        this.setState({
-          isLoading: false,
-          dataSource: ds.cloneWithRows(responseData.list),
-        }, function() {
-          // do something with new state
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .done();
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-    return (
-      <View style={{flex: 1, paddingTop: 20}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(data) => <Row {...data} />}
-          renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-          // renderHeader={() => <Header />}
-          // renderFooter={() => <Footer />}
-        />
-      </View>
-    );
-  }
-}
-AppRegistry.registerComponent('MyShoppingMall', () => ListViewDemo);
+AppRegistry.registerComponent('MyShoppingMall', () => App);
